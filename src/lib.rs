e8e176e9ec8de19f1d96833609623f07f8eb079e
Jake Martin <jm917@ic.ac.uk>
2021-02-12 11:39:29 +0000

    Refactored to use constants

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,8 @@
+const SMALL_COST: u32 = 3;
+const MEDIUM_COST: u32 = 8;
+const LARGE_COST: u32 = 15;
+const XL_COST: u32 = 25;
+
 //Types of parcel
 enum Parcel {
     Small,
@@ -10,10 +15,10 @@ impl Parcel {
     //Return the cost of the parcel
     fn get_cost(&self) -> u32 {
         match self {
-            Parcel::Small => 3,
-            Parcel::Medium => 8,
-            Parcel::Large => 15,
-            Parcel::XL => 25,
+            Parcel::Small => SMALL_COST,
+            Parcel::Medium => MEDIUM_COST,
+            Parcel::Large => LARGE_COST,
+            Parcel::XL => XL_COST,
         }
     }
 }
@@ -27,11 +32,11 @@ mod tests {
 
     #[test]
     fn single_small_parcel_cost_correct() {
-        assert_eq!(SMALL_PARCEL.get_cost(), 3);
+        assert_eq!(SMALL_PARCEL.get_cost(), SMALL_COST);
     }
 
     #[test]
     fn single_medium_parcel_cost_correct() {
-        assert_eq!(MEDIUM_PARCEL.get_cost(), 8);
+        assert_eq!(MEDIUM_PARCEL.get_cost(), MEDIUM_COST);
     }
 }
