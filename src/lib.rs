26640075c451a526ee54a4e9dbea81e62b3afc51
Jake Martin <jm917@ic.ac.uk>
2021-02-12 11:37:36 +0000

    Added other types of parcel

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
-
 //Types of parcel
 enum Parcel {
     Small,
+    Medium,
+    Large,
+    XL,
 }
 
 impl Parcel {
@@ -9,6 +11,9 @@ impl Parcel {
     fn get_cost(&self) -> u32 {
         match self {
             Parcel::Small => 3,
+            Parcel::Medium => 8,
+            Parcel::Large => 15,
+            Parcel::XL => 25,
         }
     }
 }
@@ -18,9 +23,15 @@ mod tests {
     use super::*;
 
     const SMALL_PARCEL: Parcel = Parcel::Small;
+    const MEDIUM_PARCEL: Parcel = Parcel::Medium;
 
     #[test]
     fn single_small_parcel_cost_correct() {
         assert_eq!(SMALL_PARCEL.get_cost(), 3);
+    }
+
+    #[test]
+    fn single_medium_parcel_cost_correct() {
+        assert_eq!(MEDIUM_PARCEL.get_cost(), 8);
     }
 }
